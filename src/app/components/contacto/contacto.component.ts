a77c2098886c2e5a7603b2dec3aa23cea7776642
Santiago Osuna <Santiagoosuna.com>
2025-05-16 19:57:03 -0500

    Aplicacion de api google maps y creacion de nuevo footer

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare var google: any; // Importa la API global de Google Maps
 
 @Component({
   selector: 'app-contacto',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
-export class ContactoComponent {
+export class ContactoComponent implements AfterViewInit {
 
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  initMap(): void {
+    const ubicacionCoonadoc = { lat: 4.6192, lng: -74.0721 };
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: ubicacionCoonadoc,
+    });
+
+    new google.maps.Marker({
+      position: ubicacionCoonadoc,
+      map: map,
+      title: "COONADOC",
+    });
+  }
 }
